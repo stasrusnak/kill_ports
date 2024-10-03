0fc56144908b2e7ce62d0873004ba5e13cd47e8b
stasrusnak <stasrusnak@gmail.com>
2024-10-03 17:06:33 +0300

    up

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # kill_ports
+<h3>Extension to kill processes on a specified port
+</h3>
+<a href="https://i.imgur.com/YZtbWqr.png"><img src="https://i.imgur.com/YZtbWqr.png" title="source: imgur.com" /></a>
+
+icon from open source resource https://feathericons.com/
--- extension.js
+++ extension.js
@@ -1,83 +1,72 @@
-import GLib from 'gi://GLib';
-import Gio from 'gi://Gio';
-import St from 'gi://St';
-import Clutter from 'gi://Clutter';
-import { Button } from 'resource:///org/gnome/shell/ui/panelMenu.js';
 import GObject from 'gi://GObject';
-import { panel } from 'resource:///org/gnome/shell/ui/main.js';
+import St from 'gi://St';
+import GLib from 'gi://GLib';
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
+import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
+import * as PopupMenu from 'resource:///org/gnome/shell/ui/popupMenu.js';
 
-export class KillPortIndicator extends Button {
-  _init() {
-    super._init(0, "Kill Port", false);
-
-    this.box = new St.BoxLayout({ vertical: true });
-
-    // Create entry for port number
-    this.port_entry = new St.Entry({
-      style_class: "entry",
-      text: "", // Start with empty text
-      can_focus: true
-    });
-    this.port_entry.set_style("padding: 5px;"); // Add some padding
-    this.port_entry.add_style_class_name("kill-port-entry"); // Custom class for CSS styling
-    this.box.add_child(this.port_entry);
-
-    // Create kill button
-    this.kill_button = new St.Button({
-      label: "Kill Processes",
-      style_class: "button"
-    });
-    this.kill_button.connect('clicked', () => this._kill_processes());
-    this.box.add_child(this.kill_button);
-
-    this.add_child(this.box);
-
-    // Add placeholder text through CSS
-    this.port_entry.placeholder_text = "Enter port number"; // Setting placeholder text
-  }
-
-  // Function to kill processes on the specified port
-  _kill_processes() {
-    const port = this.port_entry.get_text().trim();
-    const port_number = parseInt(port, 10);
-
-    if (!isNaN(port_number) && port_number > 0) {
-      const command = `fuser -k ${port}/tcp`;
-      const [ok, out, err, exit] = GLib.spawn_command_line_sync(command);
-
-      if (ok) {
-        Main.notify("Success", `Killed processes on port ${port_number}`);
-      } else {
-        Main.notify("Error", `Failed to kill processes: ${err}`);
-      }
-    } else {
-      Main.notify("Error", "Please enter a valid port number.");
+export default class KillPortIndicatorExtension extends Extension {
+    enable() {
+        this._indicator = new KillPortIndicator();
+        Main.panel.addToStatusArea('kill-port', this._indicator);
     }
-  }
-
-  // Stop updates
-  stop() {
-    // Any cleanup can be done here
-  }
+    disable() {
+        if (this._indicator) {
+            this._indicator.destroy();
+            this._indicator = null;
+        }
+    }
 }
 
-// Register the KillPortIndicator class
-GObject.registerClass({
-  GTypeName: 'KillPortIndicator'
-}, KillPortIndicator);
+const KillPortIndicator = GObject.registerClass(
+    class KillPortIndicator extends PanelMenu.Button {
+        _init() {
+            super._init(0, "Kill Port", false);
+            this.icon = new St.Icon({
+                style_class: 'square'
+            });
+            this.add_child(this.icon);
+            // Создание элемента меню
+            this.menuItem = new PopupMenu.PopupMenuItem('Kill Processes', { reactive: false });
+            this.menu.addMenuItem(this.menuItem);
+            // Создаем контейнер для ввода и кнопки
+            this.box = new St.BoxLayout({ vertical: true });
+            this.port_entry = new St.Entry({
+                style_class: "entry",
+                text: "",
+                can_focus: true
+            });
+            this.port_entry.set_style("padding: 5px;");
+            this.port_entry.add_style_class_name("kill-port-entry");
+            this.box.add_child(this.port_entry);
+            // Создаем кнопку "Kill Processes"
+            this.kill_button = new St.Button({
+                label: "Kill Processes",
+                style_class: "button"
+            });
+            this.kill_button.connect('clicked', () => this._kill_processes());
+            this.box.add_child(this.kill_button);
+            this.menuItem.add_child(this.box);
+            this.menuItem.label.text = 'Kill Port';
+        }
+        // Функция для убийства процессов
+        _kill_processes() {
+            const port = this.port_entry.get_text().trim();
+            const port_number = parseInt(port, 10);
 
-// Main extension class
-export default class KillPortExtension {
-  _indicator;
+            if (!isNaN(port_number) && port_number > 0) {
+                const command = `fuser -k ${port}/tcp`;
+                const [ok, out, err, exit] = GLib.spawn_command_line_sync(command);
 
-  enable() {
-    this._indicator = new KillPortIndicator();
-    panel.addToStatusArea('kill-port', this._indicator);
-  }
-
-  disable() {
-    this._indicator.stop();
-    this._indicator.destroy();
-    this._indicator = undefined;
-  }
-}
+                if (ok) {
+                    Main.notify("Success", `Killed processes on port ${port_number}`);
+                } else {
+                    Main.notify("Error", `Failed to kill processes: ${err}`);
+                }
+            } else {
+                Main.notify("Error", "Please enter a valid port number.");
+            }
+        }
+    }
+);
